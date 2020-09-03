7f98ded77921a471dd833b8a05ae490a323edc60
Dominic01prog <djs7129@psu.edu>
2020-09-03 19:23:10 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,14 @@
+// Author: Dominic Savaglio djs7129@psu.edu
+// Collaborator: Shakeb Siddiqui sms8508@psu.edu
+// Collaborator: Sneha Prem Chandran smp6705@psu.edu
+// Collaborator: Samantha Zolin saz5193@psu.edu
+#include <stdio.h>
+#include <readline/readline.h>
+#include <stdlib.h>
+int main(void) {
+  char *tempstr = readline("Enter temperature in celsius: ");
+  double temp = atof(tempstr);
+  double fahrenheit = 9.0/5.0 * temp + 32;
+  printf("%f° in Celsius is equivalent to %f° Fahrenheit.\n", temp, fahrenheit);
+  return 0;
+}
